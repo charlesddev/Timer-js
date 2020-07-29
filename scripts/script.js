8e97a4d28af7fc5325303fc5db3ed0449bda0992
Charles Dion <charlesdn222@gmail.com>
2020-07-29 15:43:48 -0400

    huge edit

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,34 @@
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
 const resetButton = document.getElementById("resetButton");
-
 let setTime = document.getElementById("time").innerHTML;
+let setTimeFixed;
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);
 setTime = parseFloat(setTime);
 
-function timeUp() {
-        setTime.toFixed(2);
-        setTime += 0.01;
-        console.log(setTime);
+
+
+function myTimer() {
+    setTime += 0.1;
+    setTimeFixed = setTime.toFixed(1);
+    document.getElementById("time").innerHTML = setTimeFixed;
 }
 
-startButton.onclick = function startTimer() {
-    window.setInterval(timeUp, 10)
+function startTimer() {
+    setInterval(myTimer, 100)
+    document.getElementById("startButton").style.visibility = "hidden";
 }
 
-stopButton.onclick = function stopTimer() {
-    clearInterval(timeUp);
+function stopTimer() {
+   clearInterval(setTime);
+   document.getElementById("startButton").style.visibility = "initial";
+}
+
+function resetTimer() {
+    clearInterval(setTime);
+    setTime = 0;
+    document.getElementById("time").innerHTML = setTime;
 }
